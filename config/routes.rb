1cd26bc368f1e03c914ecba9522a256dde30a3c5
AnattaGuo <mengjunkwok@gmail.com>
2017-07-29 14:39:39 +0800

    generate fake posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+
   devise_for :users
   get "/jquery-1" => "pages#jquery_1"
   get "/jquery-2" => "pages#jquery_2"
@@ -6,5 +8,5 @@ Rails.application.routes.draw do
   get "/jquery-4" => "pages#jquery_4"
   get "/jquery-5" => "pages#jquery_5"
 
-  root "pages#jquery_1"
+  root "posts#index"
 end
